0e4959d80f5766f82f9ff44d50b768b35ceb3351
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-27 14:48:29 -0400

    modification

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,23 @@
 const eqArrays = function (array1, array2) {
-  let identicalElm = 0;
-  for (let i = 0; i < (array1.length); i++) {
-    if (array1[i] === array2[i]) {
-      identicalElm += 1;
-    }
-  }
-  if (identicalElm === array1.length) {
-    return true;
+  if(array1.length !== array2.length) {
+    return false
   } else {
-    return false;
+   let identicalElm = 0;
+   for (let i = 0; i < array1.length; i++) {
+     if (array1[i] === array2[i]) {
+       identicalElm += 1;
+     }
+   }
+   if (identicalElm === array1.length) {
+     return true;
+   } else {
+     return false;
+   }
   }
 }
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  result = eqArrays(arrayOne, arrayTwo);
+  const result = eqArrays(arrayOne, arrayTwo);
   if (result) {
     console.log(`✅✅✅Assertion Passed: Your arrays maches perfectly!`);
   } else {
