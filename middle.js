05d7974d49810f496021b5da0c336c28f6a55667
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-27 18:53:59 -0400

    middle elements

--- middle.js
+++ middle.js
@@ -1,14 +1,13 @@
 const eqArrays = function (array1, array2) {
-  let identicalElm = 0;
-  for (let i = 0; i < (array1.length); i++) {
-    if (array1[i] === array2[i]) {
-      identicalElm += 1;
-    }
-  }
-  if (identicalElm === array1.length) {
-    return true;
+  if(array1.length !== array2.length) {
+    return false
   } else {
-    return false;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
