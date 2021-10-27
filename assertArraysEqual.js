317c7589abc45a2d219763df5c862bc60312b87a
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-27 18:48:44 -0400

    assert if the two arrays are same

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,17 +2,12 @@ const eqArrays = function (array1, array2) {
   if(array1.length !== array2.length) {
     return false
   } else {
-   let identicalElm = 0;
-   for (let i = 0; i < array1.length; i++) {
-     if (array1[i] === array2[i]) {
-       identicalElm += 1;
-     }
-   }
-   if (identicalElm === array1.length) {
-     return true;
-   } else {
-     return false;
-   }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
@@ -25,7 +20,7 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // fail
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // pass
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
