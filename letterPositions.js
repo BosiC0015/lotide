6cf886993ac004076bf3715d2f61e069bb158814
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-27 18:51:31 -0400

    count the position of a letter in a sentence

--- letterPositions.js
+++ letterPositions.js
@@ -2,17 +2,12 @@ const eqArrays = function (array1, array2) {
   if(array1.length !== array2.length) {
     return false
   } else {
-   let identicalElm = 0;
-   for (let i = 0; i < array1.length; i++) {
-     if (array1[i] === array2[i]) {
-       identicalElm += 1;
-     }
-   }
-   if (identicalElm === array1.length) {
-     return true;
-   } else {
-     return false;
-   }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
