33ebdd1688966f486614bb0c103338615ad38be7
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-28 18:59:37 -0400

    slice the array from the beginning unit callback returns truthy

---
+++ takeUntil.js
@@ -0,0 +1,53 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const result = eqArrays(arrayOne, arrayTwo);
+  if (result) {
+    console.log(`✅✅✅Assertion Passed: Your arrays maches perfectly!`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: Your arrays does not match.`);
+  }
+};
+
+// The function will return a "slice of the array with elements taken
+// from the beginning."
+// It should keep going until the callback/predicate returns a truthy value.
+
+const takeUntil = function(array, callback) {
+  let output = [];
+  array.forEach(elm => {
+    if (callback(elm)) {
+      output = array.slice(0, array.indexOf(elm));
+    }
+  });
+  return output;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+
+console.log('---');
+
+const data3 = ['just', 'for', 'testing', 'the' ,'function'];
+assertArraysEqual(takeUntil(data3, x => x === 'the'), ['just', 'for', 'testing'])
