bb4a8f6f347834c653f71e8fa671cb3e6a248ca5
Bosi Chen <chenbosi.bosi@gmail.com>
2021-10-28 20:18:17 -0400

    use callback to find the key

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+  const i = values.findIndex(value => callback(value))
+  return keys[i];
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
