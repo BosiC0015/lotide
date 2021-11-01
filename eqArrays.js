2890f0f5cecc6807799385a14113f3478b1eb040
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-01 15:48:42 -0400

    DRY and separate test

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,18 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function (array1, array2) {
-  if(array1.length !== array2.length) {
-    return false
-  } else {
-    // [1,2,3] => '123'
-    // if(array1.toString() === array2.toString()) {
-    //   return true
-    // }
+    
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
         return false;
       }
     }
     return true;
-
+  
+  // if(array1.toString() === array2.toString()) { // [1,2,3] => '123'
+  //   return true;
+  // }
   //  let identicalElm = 0;
   //  for (let i = 0; i < array1.length; i++) {
   //    if (array1[i] === array2[i]) {
@@ -33,9 +25,7 @@ const eqArrays = function (array1, array2) {
   //    return false;
   //  }
   }
-}
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+module.exports = eqArrays;
