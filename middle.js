45687b12c44d2076efe4116167dd5f496b394028
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-01 16:10:00 -0400

    modifications for DRY up and separating test

--- middle.js
+++ middle.js
@@ -1,48 +1,22 @@
-const eqArrays = function (array1, array2) {
-  if(array1.length !== array2.length) {
-    return false
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  result = eqArrays(arrayOne, arrayTwo);
-  if (result) {
-    console.log(`✅✅✅Assertion Passed: Your arrays maches perfectly!`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: Your arrays does not match.`);
-  }
-};
-
-const middle = function (array) {
-  let output = []
+const middle = function(array) {
+  let output = [];
   // one or two elements
   if (array.length <= 2) {
     return [];
   }
   // even number of elm
-  else if (array.length % 2 === 1) {
+  if (array.length % 2 === 1) {
     let n = (array.length - 1) / 2;
     output.push(array[n]);
   }
   // odd number of elm
-  else if (array.length % 2 === 0) {
+  if (array.length % 2 === 0) {
     let n = (array.length / 2);
-    output.push(array[n-1]);
+    output.push(array[n - 1]);
     output.push(array[n]);
   }
   return output;
-}
+};
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+module.exports = middle;
